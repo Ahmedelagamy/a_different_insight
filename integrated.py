3aae1ea76e1ac6bc1ecfa448c83ce9bb0f9eee17
Ahmed Mohsen El Agamy <58571185+Ahmedelagamy@users.noreply.github.com>
2023-03-10 13:45:36 +0200

    Update integrated.py

--- integrated.py
+++ integrated.py
@@ -60,7 +60,7 @@ if check_password():
     #st.image('./asset/img/acronym-color.eps')
     st.title("A Different Storyteller")
     st.sidebar.title('A Different Storyteller')
-    article = st.text_input('Enter your Text for Analysis: ')
+    article = st.text_input('Enter your Text for Analysis: ',key =123)
 
 try:
     from nltk.corpus import stopwords
